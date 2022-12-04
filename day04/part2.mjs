d677d583ad601cb2e419002ff5bd90de3d6b309e
Richard Lau <rlau@redhat.com>
2022-12-04 16:48:16 +0000

    add day 4 part 2

---
+++ day04/part2.mjs
@@ -0,0 +1,15 @@
+import { readFile } from 'node:fs/promises';
+
+const overlap = ([range1, range2]) => {
+  return (range1[0] >= range2[0] && range1[0] <= range2[1]) ||
+    (range1[1] >= range2[0] && range1[1] <= range2[1]) ||
+    (range2[0] >= range1[0] && range2[0] <= range1[1]) ||
+    (range2[1] >= range1[0] && range2[1] <= range1[1]);
+};
+
+const input = await readFile('input', { encoding: 'utf8' });
+console.log(input.split('\n').map((assignments) => {
+  return assignments.trim().split(',').map((assignment) => {
+    return assignment.split('-').map((s) => parseInt(s));
+  });
+}).filter(overlap).length);
