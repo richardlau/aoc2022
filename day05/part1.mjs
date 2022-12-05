3fc80c78bd2bb65f0c07f6027f054c397bf5034f
Richard Lau <rlau@redhat.com>
2022-12-05 20:46:37 +0000

    add day 5 part 1

---
+++ day05/part1.mjs
@@ -0,0 +1,30 @@
+import { readFile } from 'node:fs/promises';
+
+const readStacks = (lines) => {
+  const stacks = [];
+  const labels = lines[lines.length - 1];
+  const bottom = lines[lines.length - 2];
+  let position = bottom.indexOf('[');
+  while (position !== -1) {
+    const col = position + 1;
+    const n = parseInt(labels.charAt(col));
+    stacks[n] = [bottom.charAt(col)];
+    for (let l = lines.length - 3 ; l >= 0 && lines[l].charAt(col) !== ' '; l--) {
+      stacks[n].unshift(lines[l].charAt(col));
+    }
+    position = bottom.indexOf('[', col);
+  }
+  return stacks;
+};
+
+const input = await readFile('input', { encoding: 'utf8' });
+const [ stacklines, steps ] = input.split('\n\n');
+const stacks = readStacks(stacklines.split('\n'));
+const moveRE = /move (\d+) from (\d+) to (\d+)/;
+for (const step of steps.split('\n')) {
+  const [ _, amount, from, to ] = step.match(moveRE);
+  for (let i = 0; i < amount; i++) {
+    stacks[to].unshift(stacks[from].shift());
+  }
+}
+console.log(stacks.map((s) => s[0]).join(''));
