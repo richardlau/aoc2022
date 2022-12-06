04745bc8467eb9ba14fe84932b00cb2f5c3bb7b4
Richard Lau <rlau@redhat.com>
2022-12-06 15:36:37 +0000

    add day 6 part 2

---
+++ day06/part2.mjs
@@ -0,0 +1,20 @@
+import { readFile } from 'node:fs/promises';
+
+const isMarker = (s, start, end) => {
+  for (let i = start; i < end - 1; i++) {
+    for (let j = i + 1; j < end; j++) {
+      if (s.charCodeAt(i) === s.charCodeAt(j)) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+const input = await readFile('input', { encoding: 'utf8' });
+const markerLength = 14;
+let marker = markerLength;
+while (!isMarker(input, marker - markerLength, marker)) {
+  marker++
+};
+console.log(marker);
