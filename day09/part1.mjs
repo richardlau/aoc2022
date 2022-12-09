8a75085f3e5aef840ac7b99a1e640a71ea9b20eb
Richard Lau <rlau@redhat.com>
2022-12-09 18:09:22 +0000

    refactor day 9 part 1 for multiple knots

--- day09/part1.mjs
+++ day09/part1.mjs
@@ -1,10 +1,10 @@
 import { readFile } from 'node:fs/promises';
 
-const coordinates = (x, y) => `x${x}y${y}`;
+const coordinates = ([x, y]) => `x${x}y${y}`;
 const isAdjacent = (hx, hy, tx, ty) => {
   return [-1, 0, 1].includes(hx - tx) && [-1, 0, 1].includes(hy - ty);
 };
-const moveTail = (hx, hy, tx, ty) => {
+const moveKnot = ([hx, hy], [tx, ty]) => {
   const dx = hx - tx;
   const dy = hy - ty;
   if (isAdjacent(hx, hy, tx, ty)) {
@@ -14,48 +14,26 @@ const moveTail = (hx, hy, tx, ty) => {
   let ry = ty;
   rx += dx === 0 ? 0 : dx / Math.abs(dx);
   ry += dy === 0 ? 0 : dy / Math.abs(dy);
-  const newcoords = coordinates(rx, ry);
-  tailPositions.set(newcoords, 1 + (tailPositions.get(newcoords) ?? 0));
   return [rx, ry];
 };
 
 const input = await readFile('input', { encoding: 'utf8' });
-let hx = 0;
-let hy = 0;
-let tx = 0;
-let ty = 0;
+let knots = [...new Array(2)].map((_) => [0, 0]);
+
 const tailPositions = new Map();
-tailPositions.set(coordinates(tx, ty), 1);
+tailPositions.set(coordinates(knots[knots.length - 1]), 1);
 for (const move of input.split(/\r\n|\r|\n/)) {
   const [ direction, steps ] = move.split(' ')
                                .map((v, i) => i === 1 ? Number(v) : v);
-  switch (direction) {
-    case 'D':
-      for (let i = 0; i < steps; i++) {
-        hy++;
-        [tx, ty] = moveTail(hx, hy, tx, ty);
-      }
-      break;
-    case 'L':
-      for (let i = 0; i < steps; i++) {
-        hx--;
-        [tx, ty] = moveTail(hx, hy, tx, ty);
-      }
-      break;
-    case 'R':
-      for (let i = 0; i < steps; i++) {
-        hx++;
-        [tx, ty] = moveTail(hx, hy, tx, ty);
-      }
-      break;
-    case 'U':
-      for (let i = 0; i < steps; i++) {
-        hy--;
-        [tx, ty] = moveTail(hx, hy, tx, ty);
-      }
-      break;
-    default:
-      console.error(`Unable to parse move '${move}'`);
-  };
+  const dx = direction === 'R' ? 1 : direction === 'L' ? -1 : 0;
+  const dy = direction === 'D' ? 1 : direction === 'U' ? -1 : 0;
+  for (let i = 0; i < steps; i++) {
+    knots[0] = [knots[0][0] + dx, knots[0][1] + dy];
+    for (let j = 1; j < knots.length; j++) {
+      knots[j] = moveKnot(knots[j - 1], knots[j]);
+    }
+    const newcoords = coordinates(knots[knots.length - 1]);
+    tailPositions.set(newcoords, 1 + (tailPositions.get(newcoords) ?? 0));
+  }
 };
 console.log([...tailPositions.keys()].length);
